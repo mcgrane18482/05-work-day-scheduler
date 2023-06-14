59445721cf2ff26d44df44024fd3b50b08e41720
Fruit Juice <c.erinmcgrane1@gmail.com>
2023-06-14 17:31:59 -0400

    added logic for determining past present and future versus currentHour

--- script.js
+++ script.js
@@ -3,18 +3,8 @@
 // in the html.
 var main = $('#main');
 var currentHour = dayjs().hour()
+$("#currentDay").text(dayjs().format("dddd D MMMM, YYYY"))
 
-var hours = {
-  9: 'nine',
-  10: 'ten',
-  11: 'eleven',
-  12: 'twelve',
-  13: 'one',
-  14: 'two',
-  15: 'three',
-  16: 'four',
-  17: 'five',
-}
 
 $(function () {
   $(".saveBtn").on("click", function(event){
@@ -34,23 +24,17 @@ $("#hour-15 .description").val(localStorage.getItem("hour-15"))
 $("#hour-16 .description").val(localStorage.getItem("hour-16"))
 $("#hour-17 .description").val(localStorage.getItem("hour-17"))
 
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+$(".time-block").each(function(){
+  var blockTime = parseInt($(this).attr("id").split("-")[1])
+  if(blockTime<currentHour){
+    $(this).addClass("past")
+  }else if(blockTime === currentHour){
+    $(this).removeClass("past")
+    $(this).addClass("present")
+  }else{
+    $(this).removeClass("past")
+    $(this).removeClass("present")
+    $(this).addClass("future")
+  }
+})
 });
